476513d39eb0da4a7390c9ba4b77dbf4c240988e
meganemura <mura2megane@gmail.com>
2013-10-27 01:22:30 +0900

    Initial commit

---
+++ spec/main_spec.rb
@@ -0,0 +1,9 @@
+describe "Application 'joybox-tile-game'" do
+  before do
+    @app = UIApplication.sharedApplication
+  end
+
+  it "has one window" do
+    @app.windows.size.should == 1
+  end
+end
