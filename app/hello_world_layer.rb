64dd653845af176b820166f516a1d50966674c74
meganemura <mura2megane@gmail.com>
2013-10-27 20:55:06 +0900

    Part1: Tiled Object Layers and Setting Tile Map Position

--- app/hello_world_layer.rb
+++ app/hello_world_layer.rb
@@ -5,8 +5,23 @@ class HelloWorldLayer < Joybox::Core::Layer
   scene
 
   def on_enter
-    tile_map = TileMap.new(:file_name => 'tile_map.tmx')
-    tile_layer = tile_map.tile_layers['Background']
+    @tile_map = TileMap.new(:file_name => 'tile_map.tmx')
+    tile_layer = @tile_map.tile_layers['Background']
     self << tile_layer
+
+    tile_object = @tile_map.object_layers['Objects']
+    spawn_point = tile_object.objectNamed('SpawnPoint')
+    p spawn_point.inspect
+    p spawn_point.class
+    x = spawn_point["x"]
+    y = spawn_point["y"]
+
+    player = Sprite.new(:file_name => 'arts/Player.png')
+    player.position = jbp(x, y)
+
+    self << player
+
+    center_at(player.position)
   end
+
 end
