f6d0ff190119d14e597e82b9be40abacf4909e3e
meganemura <mura2megane@gmail.com>
2013-10-31 00:37:25 +0900

    Part2: Tiled Maps and Collisions

--- app/hello_world_layer.rb
+++ app/hello_world_layer.rb
@@ -9,6 +9,9 @@ class HelloWorldLayer < Joybox::Core::Layer
     tile_layer = @tile_map.tile_layers['Background']
     self << tile_layer
 
+    @meta = @tile_map.tile_layers['Meta']
+    @meta.visible = false
+
     tile_object = @tile_map.object_layers['Objects']
     spawn_point = tile_object.objectNamed('SpawnPoint')
     x = spawn_point["x"]
@@ -47,11 +50,32 @@ class HelloWorldLayer < Joybox::Core::Layer
           position.y <= (@tile_map.tiles_size.height * @tile_map.tile_size.height) &&
           position.x >= 0 &&
           position.y >= 0)
-        @player.position = position
+        set_player_position(position)
       end
 
       center_at(@player.position)
     end
+  end
+
+  def set_player_position(position)
+    tile_coord = tile_coord_for_position(position)
+    tile_gid = @meta.tileGIDAt(tile_coord)
+    if tile_gid
+      properties = @tile_map.propertiesForGID(tile_gid)
+      if properties
+        collision = properties['Collidable']
+        if collision && collision == 'True'
+          return
+        end
+      end
+    end
+    @player.position = position
+  end
+
+  def tile_coord_for_position(position)
+    x = position.x / @tile_map.tile_size.width
+    y = ((@tile_map.tiles_size.height * @tile_map.tile_size.height) - position.y) / @tile_map.tile_size.height
+    jbp(x.to_i, y.to_i)
   end
 
 end
